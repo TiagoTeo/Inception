b65f67269309d0e9fac63040ed798aff33fd4aeb
TiagoTeo <manueltiagoteodoro@gmail.com>
2024-03-19 17:00:14 +0000

    not tested

---
+++ requirements/nginx/tools/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/privatekey.key -out /etc/ssl/certs/certificate.crt -subj "/C=PT/L=Lisboa/O=42Lisboa/OU=Student/CN=mtiago-s.42.fr"
+
+
+echo "
+server {
+    listen 443 ssl default_server;
+    listen [::]:443 ssl default_server;
+
+    server_name mtiago-s.42.fr www.mtiago-s.42.fr;
+
+    ssl_certificate /etc/ssl/certs/certificate.crt;
+    ssl_certificate_key /etc/ssl/private/privatekey.key
+    ssl_protocols TLSv1.3;" > /etc/nginx/sites-available/default
+
+
+# echo '
+
+#     index index.php;
+#     root /var/www/html;
+
+#     location ~ [^/]\.php(/|$) { 
+#             try_files $uri =404;
+#             fastcgi_pass wordpress:9000;
+#             include fastcgi_params;
+#             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+#         }
+# } ' >>  /etc/nginx/sites-available/default
+
+
+# nginx -g "daemon off;"
